76f754dacf9beb153be5e8b03bf28764fa4705d6
Benjamin Van Heest <b.vanheest@gmail.com>
2018-05-27 13:07:17 -0500

    Add router, auth route and controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var db = require('./db');
+var router = require('./router');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controllers/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  register() {
+
+  },
+
+  authenticate() {
+
+  },
+}
